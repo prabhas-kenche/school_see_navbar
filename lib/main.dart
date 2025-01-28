19045f7967143e33f60279b3a4427dffdbab4baa
Mohd Irfan <irfu.aa143@gmail.com>
2025-01-28 22:58:45 +0530

    Integrated OTPInputField into OTPVerificationScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,21 @@
 // lib/main.dart
 import 'package:flutter/material.dart';
-import 'package:school_see_navbar/homepage.dart'; // Import HomePage correctly
+import 'package:school_see_navbar/screens/otp_verification.dart';
+
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(), // Use HomePage as the starting point
+      title: 'OTP UI',
+      home: OTPVerificationScreen(),
     );
   }
 }
